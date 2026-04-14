128a371088609b8fb0d567c7b858ea02962927fe
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-04-14 15:12:24 +0600

    hero section card added

--- src/pages/home/HeroCard.jsx
+++ src/pages/home/HeroCard.jsx
@@ -2,23 +2,23 @@ import React from 'react';
 
 const HeroCard = () => {
     return (
-        <div className='container mx-auto grid grid-cols-4'>
-            <div>
-                <div>
-                    <h2>10</h2>
-                    <p>Total Friends</p>
+        <div className='max-w-[80%] mx-auto'>
+            <div className=' grid grid-cols-2 md:grid-cols-4 gap-5 mb-15'>
+                <div className='flex flex-col items-center justify-center p-[32px] shadow-xl rounded-2xl'>
+                    <h2 className='font-bold'>10</h2>
+                    <p className='text-[#64748B]'>Total Friends</p>
                 </div>
-                <div>
-                    <h2>3</h2>
-                    <p>On Track</p>
+                <div className='flex flex-col items-center justify-center p-[32px] shadow-xl rounded-2xl'>
+                    <h2 className='font-bold'>3</h2>
+                    <p className='text-[#64748B]'>On Track</p>
                 </div>
-                <div>
-                    <h2>6</h2>
-                    <p>Need Attention</p>
+                <div className='flex flex-col items-center justify-center p-[32px] shadow-xl rounded-2xl'>
+                    <h2 className='font-bold'>6</h2>
+                    <p className='text-[#64748B]'>Need Attention</p>
                 </div>
-                <div>
-                    <h2>12</h2>
-                    <p>Interactions This Month</p>
+                <div className='flex flex-col items-center justify-center p-[32px] shadow-xl rounded-2xl'>
+                    <h2 className='font-bold'>12</h2>
+                    <p className='text-[#64748B]'>Interactions This Month</p>
                 </div>
             </div>
         </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './hero';
+import HeroCard from './HeroCard';
 
 const Home = () => {
     return (
         <div>
             <Hero></Hero>
+            <HeroCard></HeroCard>
         </div>
     );
 };
