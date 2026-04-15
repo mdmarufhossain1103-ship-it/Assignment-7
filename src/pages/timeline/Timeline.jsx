04203d17494bc06f6cee2711d50f759d39343449
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-04-15 08:48:33 +0600

    Filter button added

--- src/pages/timeline/Timeline.jsx
+++ src/pages/timeline/Timeline.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CallContextDetails from './CallContextDetails';
 import TextContextDetails from './TextContextDetails';
 import VideoContextDetails from './VideoContextDetails';
@@ -6,23 +6,25 @@ import VideoContextDetails from './VideoContextDetails';
 
 
 const Timeline = () => {
+    const [filter, setFilter] = useState('All')
     return (
-            <div className='max-w-[80%] mx-auto my-20'>
-                <div>
-                    <h2 className='text-3xl font-bold mb-3'>Timeline </h2>
-                    <select defaultValue="Pick a color" className="select">
-                        <option disabled={true}>Filter timeline</option>
-                        <option>Call</option>
-                        <option>Text</option>
-                        <option>Video</option>
-                    </select>
-                    <div className='mt-5'>
-                        <CallContextDetails></CallContextDetails>
-                        <TextContextDetails></TextContextDetails>
-                        <VideoContextDetails></VideoContextDetails>
-                    </div>
+        <div className='max-w-[80%] mx-auto my-20'>
+            <div>
+                <h2 className='text-3xl font-bold mb-3'>Timeline </h2>
+                <select value={filter} onChange={(e) => setFilter(e.target.value)} defaultValue="Pick a color" className="select">
+                    <option disabled={true}>Filter timeline</option>
+                    <option value="All">Show All</option>
+                    <option value='Call'>Call</option>
+                    <option value='Text'>Text</option>
+                    <option value='Video'>Video</option>
+                </select>
+                <div className='mt-5'>
+                    {(filter === 'All' || filter === 'Call') && <CallContextDetails></CallContextDetails>}
+                    {(filter === 'All' || filter === 'Text') && <TextContextDetails></TextContextDetails>}
+                    {(filter === 'All' || filter === 'Video') && <VideoContextDetails></VideoContextDetails>}
                 </div>
             </div>
+        </div>
     );
 };
 
